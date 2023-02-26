1640528c322eadc3c045ce67a7148b0afbe933a9
caden99 <123427611+caden99@users.noreply.github.com>
2023-02-26 07:33:02 +0000

    hw partial

--- test/examples/05_functions/05_functions_tests.cpp
+++ test/examples/05_functions/05_functions_tests.cpp
@@ -1,6 +1,8 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "val_ref.h"
+#include "default.h"
+
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
@@ -13,5 +15,23 @@ TEST_CASE("Test value parameter", "knowns as a copy parameter")
 
 	val_ref_params(num, num1, num2);
 	REQUIRE(num == 10);
-	REQUIRE(num == 20);
+	REQUIRE(num == 10);
 }
+
+
+TEST_CASE("Verify get gross pay default parameters")
+{
+	REQUIRE(get_gross_pay() == 600);
+	REQUIRE(get_gross_pay(40) == 800);
+	REQUIRE(get_gross_pay(40, 10) == 400);
+}
+
+TEST_CASE("Verify get gross pay with double")
+{
+	REQUIRE(get_gross_pay(40, 7.5) == 300);
+}
+TEST_CASE("Verify overloaded get gross pay")
+{
+	REQUIRE(get_gross_pay(40, 10, 100) == 401);
+}
+
