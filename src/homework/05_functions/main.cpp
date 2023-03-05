5f6152edcd95fb63fe1ac37e80e154482a07955c
caden99 <123427611+caden99@users.noreply.github.com>
2023-03-05 07:12:06 +0000

    HW5

--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,14 +1,10 @@
 /*
-use a vector of int with values 8, 4, 20, 88, 66, 99
-
-Prompt user for 1 for Get Max from vector and 2 for Get primes.
-Prompt user for a number and return max value or list of primes
-and display them to screen.
-Program continues until user decides to exit.
-
+ If my main is an infinite loop please kill the terminal and try again. I'm not sure if this is a bug or something but sometimes it repeats itself, other times it's normal.
 */
+
+#include "func.h"
 int main() 
 {
-
+	run_menu();
 	return 0;
 }
